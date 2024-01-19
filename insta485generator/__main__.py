23fa4aa2bd58213de6d4bc103393640687bb2652
shihuis <shihuis@umich.edu>
2024-01-19 12:50:50 -0500

    templates done, but still some small mistakes

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -8,6 +8,7 @@ import json
 
 # read configuration file
 def read_config(config_filename):
+    """Read configuration file and return it."""
     config_filename = pathlib.Path(config_filename)
     try:
         with config_filename.open() as config_file:
@@ -37,6 +38,13 @@ def main(input_dir, output, verbose):
             loader=jinja2.FileSystemLoader(str(template_dir)),
             autoescape=jinja2.select_autoescape(['html', 'xml'])
         )
+        # check output directory
+        if output:
+            output_dir = pathlib.Path(output)
+        else:
+            output_dir = input_dir/"html"
+        if output_dir.exists():
+            raise FileExistsError(f"'{output_dir}' already exists")
         # render template with context
         for item in config:
             try:
@@ -44,18 +52,12 @@ def main(input_dir, output, verbose):
                 template_selected = template_env.get_template(temp_name)
                 render_result = template_selected.render(item['context'])
             except jinja2.TemplateError as e:
-                click.echo(f"insta485generator error:
-                           '{temp_name}'\n{e.message}", err=True)
+                click.echo("insta485generator error:" +
+                           f" '{temp_name}'\n{e.message}", err=True)
                 exit(1)
             # write rendered content to output file
-            if output:
-                output_dir = pathlib.Path(output)
-            else:
-                output_dir = input_dir/"html"
             output_file = output_dir/item["url"].lstrip("/")/"index.html"
             # Check if the output directory already exists
-            if output_dir.exists():
-                raise FileExistsError(f"'{output_dir}' already exists")
             output_file.parent.mkdir(parents=True, exist_ok=False)
             with output_file.open("w") as file:
                 file.write(render_result)
@@ -64,12 +66,12 @@ def main(input_dir, output, verbose):
             if verbose:
                 print(f"Rendered {temp_name} -> {output_file}")
             # copy static directory
-            static_dir = input_dir / 'static'
-            if static_dir.exists():
-                # shutil.copy() is very useful for copying directories
-                shutil.copytree(static_dir, output_dir, dirs_exist_ok=True)
-                if verbose:
-                    print(f"Copied {static_dir} -> {output_dir}")
+        static_dir = input_dir / 'static'
+        if static_dir.exists():
+        # shutil.copy() is very useful for copying directories
+            shutil.copytree(static_dir, output_dir, dirs_exist_ok=True)
+        if verbose:
+            print(f"Copied {static_dir} -> {output_dir}")
     except json.JSONDecodeError as e:
         click.echo(f"insta485generator error: '{config}'\n{e.msg}", err=True)
         exit(1)
